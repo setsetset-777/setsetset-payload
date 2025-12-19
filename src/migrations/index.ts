020c19759fab2a4323d89dae95dafe3090647acf
Loïc Hamet <1453246+loic-h@users.noreply.github.com>
2025-12-19 10:18:44 +0100

    Update migration name

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20251130_214722_db_name from './20251130_214722_db_name';
+import * as migration_20251130_214722_services_page_slug  from './20251130_214722_services-page-slug';
 
 export const migrations = [
   {
-    up: migration_20251130_214722_db_name.up,
-    down: migration_20251130_214722_db_name.down,
+    up: migration_20251130_214722_services_page_slug .up,
+    down: migration_20251130_214722_services_page_slug .down,
     name: '20251130_214722_db_name'
   },
 ];
